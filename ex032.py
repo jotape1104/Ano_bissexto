0280a663aa513fd47a2e3286fdbad4ae3d51f841
João Pedro <129243612+jotape1104@users.noreply.github.com>
2023-08-06 22:03:45 -0300

    Add files via upload

---
+++ ex032.py
@@ -0,0 +1,8 @@
+from datetime import date
+ano = int(input('Digite um ano qualquer ou se quiser analisar o ano atual digite "0": '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} bissexto!'.format(ano))
+else:
+    print('O ano {} não é bissexto!'.format(ano))
